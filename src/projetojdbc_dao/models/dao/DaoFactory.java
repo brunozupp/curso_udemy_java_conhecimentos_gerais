3e93657cb043f466fc690443bb8ee675d9ff1a80
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-07-26 23:46:45 -0300

    SellerDaoJDBC e DaoFactory

---
+++ src/projetojdbc_dao/models/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package projetojdbc_dao.models.dao;
+
+import projetojdbc_dao.models.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+}
